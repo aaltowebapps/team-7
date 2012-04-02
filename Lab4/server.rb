a81ad3f847e7818645e09073976efe8f5cf8aba8
Antti Risteli <antti.risteli@aalto.fi>
2012-04-02 16:51:13 +0300

    Added Lab4 base

---
+++ Lab4/server.rb
@@ -0,0 +1,34 @@
+# stop ssl errors when using instagram
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+require 'sinatra'
+require 'sinatra/reloader' 
+require 'base64'
+require 'json'
+require 'open-uri'
+
+INSTAGRAM_API_KEY = "ffc1e462cbc8442d9247ea1b32fb45e1"
+
+get '/' do
+  redirect '/index.html'
+end
+
+# paths for instagram API
+# need to have a server because of origin policy
+get '/places/:lat/:lng/' do |lat, lng|
+  content_type :json
+  open("https://api.instagram.com/v1/locations/search?lat=#{lat}&lng=#{lng}&client_id=#{INSTAGRAM_API_KEY}")
+end
+
+get '/images/:id/' do |id|
+  content_type :json
+  open("https://api.instagram.com/v1/locations/#{id}/media/recent/?client_id=#{INSTAGRAM_API_KEY}")
+end
+
+# acts as a wrapper for getting image data
+# url is in base64
+get '/get_url/:url/' do |url|
+  content_type :jpg
+  url = Base64.decode64(url).gsub("\\/", "/")
+  open(url)
+end
