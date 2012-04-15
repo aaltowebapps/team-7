a43199ecfc7eae82dd6aaf73efaf1f3d3a757b69
Sampsa Toivanen <sampsa.toivanen@iki.fi>
2012-04-15 13:01:58 +0300

    API serves test data (converted from tilaluntti output) as building data for now.

---
+++ scripts/tilaluntti_output_converter.rb
@@ -0,0 +1,43 @@
+require 'json'
+require 'time'
+
+# Converts tilaluntti_output format to transmission format
+
+contents = File.open('tilaluntti_output.json', 'rb').read
+
+result = Hash.new
+
+result['last_updated'] = Time.new.iso8601
+result['buildings'] = []
+
+input = JSON.parse(contents)
+
+input.each do |b|
+  building = Hash.new
+  building['name'] = b['name']
+  building['latitude'] = b['latitude']
+  building['longitude'] = b['longitude']
+  building['floor_ids'] = []
+  building['rooms'] = []
+
+  b['rooms'].each do |r|
+    name = r['room_name']
+    weboodi_code = r['room_code']
+    
+    room = Hash.new
+    room['room_id'] = weboodi_code
+    room['name'] = name
+    room['keywords'] = []
+    room['keywords'] << name
+    room['keywords'] << weboodi_code
+    room['map_data'] = nil
+    
+    building['rooms'] << room
+  end
+  
+  result['buildings'] << building
+end
+
+File.open('building_data.json', 'wb') do |f|
+  f.puts JSON.pretty_generate(result)
+end
--- server/building.rb
+++ server/building.rb
@@ -6,48 +6,11 @@ require_relative 'building_classes.rb'
 # Contains logic for accessing building and floormap data.
 #
 
-def building_data
-  # TODO: replace with datamapper configuration and fetching logic
-  building = Building.new
-  
-  building.name='T-talo'
-  building.latitude = 60.15
-  building.longitude = 24.5
-  building.floor_ids = [11, 12]
-
-  building.rooms = []
-  building.rooms[0] = Room.new
-  building.rooms[0].name = 'T1-sali'
-  building.rooms[0].room_id = 21
-  building.rooms[0].keywords = ['T1-sali', 'Sali T1']
-  building.rooms[0].map_data = {'floor_id' => 11, 'map_x' => 0.25, 'map_y' => 0.7}
-
-  building.rooms[1] = Room.new
-  building.rooms[1].name = 'T2-sali'
-  building.rooms[1].room_id = 22
-  building.rooms[1].keywords = ['T2-sali', 'Sali, jonka map_data puuttuu, koska kerroksesta ei ole tietoa!']
-
-  floor1 = Floor.new
-  floor1.floor_id = 11
-  floor1.name = '1'
-  floor1.map_image = 'ttalo-1-kerros.png'
-  floor1.width = 500
-  floor1.height = 600
-
-  floor2 = Floor.new
-  floor2.floor_id = 21
-  floor2.name = '2'
-  floor2.map_image = 'ttalo-2-kerros.png'
-  floor2.width = 500
-  floor2.height = 600
- 
-  data = {
-    'last_updated' => '2012-04-08T12:00:00Z',
-    'buildings' => [building],
-    'floors' => [floor1, floor2]
-  }
-  data.to_json  
-
+# Returns all building data in JSON format (see wiki, TODO: URL).
+def building_data(timestamp = nil)
+  # TODO: replace temporary static test data with live data read from DB
+  contents = File.open('building_data.json', 'rb').read
+  return contents;
 end
 
 # Returns available floormap names. If timestamp is given, only files newer
